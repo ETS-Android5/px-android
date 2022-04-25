d9bf40d17347fbe576df9495af42eda64a04d1f4
Amauri Rodrigues <amauri.rodrigues@mercadolivre.com>
2022-04-25 14:45:07 -0300

    [PXN-3417] remove unnecessary parcelable

--- px-services/src/main/java/com/mercadopago/android/px/model/Tokenization.kt
+++ px-services/src/main/java/com/mercadopago/android/px/model/Tokenization.kt
@@ -1,13 +1,9 @@
 package com.mercadopago.android.px.model
 
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
 data class Tokenization(
     val lastModifiedDate: String,
     val tokenizationId: String,
     val status: String,
     val bindingTokenizationId: String,
     val boundDevices: List<String>
-) : Parcelable
+)
