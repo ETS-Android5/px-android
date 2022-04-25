862c1b9e2213f32be8a89fd77175c9b75d5603a8
Amauri Rodrigues <amauri.rodrigues@mercadolivre.com>
2022-04-25 09:40:41 -0300

    [PXN-3417] fix tokenization data

--- px-services/src/main/java/com/mercadopago/android/px/model/Tokenization.kt
+++ px-services/src/main/java/com/mercadopago/android/px/model/Tokenization.kt
@@ -2,13 +2,12 @@ package com.mercadopago.android.px.model
 
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
-import java.io.Serializable
 
 @Parcelize
-class Tokenization(
-    val last_modified_date: String,
-    val tokenization_id: String,
+data class Tokenization(
+    val lastModifiedDate: String,
+    val tokenizationId: String,
     val status: String,
-    val binding_tokenization_id: String,
-    val bound_devices: String?
-) : Parcelable, Serializable
+    val bindingTokenizationId: String,
+    val boundDevices: List<String>
+) : Parcelable
